4f489052a808ba25acde4f80cf5b59562b9281a6
clidrobalo <clidrobalo@gmail.com>
2020-01-08 01:46:28 +0000

    Recomendação velocidade com altura 0 concluido

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,49 @@
-import java.math.RoundingMode
+
 import kotlin.math.*
 
-fun main(args: Array<String>) {
+//-------------------------------------CONSTANTES----------
+val larguraGrafico : Int = 75
+val alturaGrafico : Int = 30
+val grafico = Chart(larguraGrafico, alturaGrafico)
+
+public fun main(args: Array<String>) {
 
     //-------------------------------------CONSTANTES----------
-    val larguraGrafico : Int = 75
-    val alturaGrafico : Int = 30
-    val alcanceDefault : Double = 130.0
     val g = 9.807
-    val grafico = Chart(larguraGrafico, alturaGrafico)
+    val alcanceDefault : Double = 130.0
 
 
+    var opcao =  menuPrincipal()
+    when(opcao) {
+        "1" -> simulacao(g, alcanceDefault)
+        "3" -> configuracao()
+        "0" -> terminar()
+        else -> {
+            println("|-----------------------------------------------------------|")
+            println("|- Opção invalida. Tente novamente.")
+            println("|-----------------------------------------------------------|")
+            menuPrincipal()
+        }
+    }
+}
+
+fun menuPrincipal()  : String {
+    println()
+    println("|***********************************************************|")
+    println("|********    Simulação lançamento projétil     *************|")
+    println("|************************************************************")
+    println("|-----------------------------------------------------------|")
+    println("|[1] Iniciar simulação")
+    println("|[3] Configuração")
+    println("|[0] Encerrar")
+    println("|-----------------------------------------------------------|")
+    print("|Escolha uma opcao: ")
+    var opcao = readLine()!!.toString()
+    return opcao
+}
+
+fun simulacao(g : Double, alcanceDefault: Double) {
     var opcao : String
-
-    println()
-    println("|************************************************************")
-    println("*************               Exemplo            **************")
-    println("|************************************************************")
-    println()
 
     do {
         var x :Double = 0.0
@@ -73,18 +99,23 @@ fun main(args: Array<String>) {
         opcao = readLine()!!.toString()
 
     }while(opcao.toLowerCase() == "s");
+    menuPrincipal()
+}
 
-    println("|------------------------------------------------------------")
-    println("|- Simulação terminada.")
-    println("|------------------------------------------------------------")
+fun configuracao() {
 
 }
 
+fun terminar() {
+    println("|------------------------------------------------------------")
+    println("|- Simulação terminada.")
+    println("|------------------------------------------------------------")
+    System.exit(0)
+}
 /*
     CALCULAR E OBTER TEMPO DE VOO
  */
 fun obterTempoVoo(v0y : Double, y0 : Double, g : Double) : Double{
-    //return (v0y + sqrt(v0y * v0y + 2 * g * y0)) / g
     return (v0y / g) + sqrt((v0y/g)*(v0y/g) + ((2*y0)/g))
 }
 
@@ -113,66 +144,8 @@ fun analisarDistanciaHorizontal(alcance : Double, alcanceDefault : Double, ang: 
     CALCULAR E OBTER VELOCIDADE RECOMENDADA PARA ALCANÇAR O OBJETIVO (alcalceDefault)
  */
 fun obterVelocidadeRecomendada(ang : Double, h: Double, alcanceDefault:Double, g : Double): Double {
-    //var ang = Math.toRadians(35.0)
-    println("--> " + cos(ang))
-    //Vo = (0.5*Ay*(X/cosθ)²)
-    // / (10+(sinθ)*(X/cosθ))
-    var numerador = 0.5 * 9.8 * Math.pow(750/cos(ang), 2.0)
-    var denominador = 10 + sin(ang) * (750/ cos(ang))
-    //var v0 =  numerador / denominador
 
-    //var v0 = sqrt((9.8 * 750) / sin(2*ang)) // Essa formula funciona
     var v0 = sqrt((g * alcanceDefault) / sin(2*ang))
 
-//    var dividendo = alcanceDefault * g
-//    var divisor = sin(ang)
-//    //garantir que o divisor sempre seja positiv0 antes de realizar raiz quadrada
-//    if(divisor < 0){
-//        divisor *= -1
-//    }
-//    //var velocidade = sqrt(dividendo/divisor)//.toBigDecimal().setScale(1, RoundingMode.UP).toDouble()
-//    var velocidade = (1/cos(ang)) * sqrt((0.5*g*alcanceDefault*alcanceDefault)/alcanceDefault * tan(ang))
     return v0
-}
-
-//fun obterVelocidadeRecomendada(ang : Double, h: Double): Double { // todo ainda não funciona
-//
-//    var velocidade = 130/(cos(ang)*calculaT(h, ang))
-//
-//    println(velocidade)
-//    return velocidade
-//}
-
-fun calculaT (h: Double, ang: Double, g : Double) : Double{
-    // (((g * t²)/2 - h) / sen &) * cos & * t =  130
-
-    val a = (2-h)*g
-    val b = cos(ang)
-    val c = -130*sin(ang)
-    var t1: Double
-    var t2: Double
-
-    // Báskra:
-
-    var delta = (b*b)+((-4)*(a)*(c));
-
-    t1 = (-b + sqrt(delta)) / (2*a)
-    t2 = (-b - sqrt(delta)) / (2*a)
-
-    println("T2: "+t1)
-    println("T1: "+t2)
-
-    var t : Double
-
-    if (t1==t2){
-        t = t1;
-    } else {
-        if (t1>t2){
-            t = t1
-        } else {
-            t=t2
-        }
-    }
-    return t;
-
 }
